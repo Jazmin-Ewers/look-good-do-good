d7958d7c2b246721f98ab5cbaefe97aef8de9674
Jazmin-Ewers <jazminewers11@gmail.com>
2022-02-09 22:44:45 -0800

    Add UI to make clothing item card clickable

--- src/components/ClothingListItem/ClothingListItem.jsx
+++ src/components/ClothingListItem/ClothingListItem.jsx
@@ -1,15 +1,18 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
 
 export default function ClothingListItem({clothingItem}) {
   return (
     <div className="ClothingListItem">
+    <Link to={`/movies/${clothingItem._id}`} className="movie-link">
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={clothingItem.mainimageurl} />
         <Card.Body>
             <Card.Text>{clothingItem.description}</Card.Text>
         </Card.Body>
     </Card>
+    </Link>
     </div>
   );
 }
