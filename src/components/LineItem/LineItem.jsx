339d56cd0f28b4e9adc07f9bab2311b1f2faf700
Jazmin-Ewers <jazminewers11@gmail.com>
2022-02-11 12:32:45 -0800

    Add table and checkout button to Cart Page

--- src/components/LineItem/LineItem.jsx
+++ src/components/LineItem/LineItem.jsx
@@ -2,15 +2,23 @@ import Card from 'react-bootstrap/Card';
 
 export default function LineItem({ lineItem, isPaid}) {
   return (
-    <tr>
-    <td> 
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={lineItem.mainimageurl} />
-    </Card>
-    </td>
-    <td>{lineItem.qty}</td>
-    <td>{lineItem.item.price}</td>
-    <td>{lineItem.exitPrice}</td>
-  </tr>
+    <div className="table-row">
+    <div className="CartPageImg">lemi<img src={lineItem.item.mainimageurl}></img></div>
+    <div className="row-item">{lineItem.item.price}</div>
+    <div className="row-item">{lineItem.qty}</div>
+    <div className="row-item">{lineItem.exitPrice}</div>
+    </div>
   );
 }
+
+  {/* //   <tr>
+  //   <td> 
+  //   <Card style={{ width: '18rem' }}>
+  //     <Card.Img variant="top" src={lineItem.mainimageurl} />
+  //   </Card>
+  //   </td>
+  //   <td>{lineItem.qty}</td>
+  //   <td>{lineItem.item.price}</td>
+  //   <td>{lineItem.exitPrice}</td>
+  // </tr>
+  ); */}
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
 import LineItem from '../../components/LineItem/LineItem';
 import * as ordersAPI from '../../utilities/orders-api';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import './CartPage.css';
 
 export default function CartPage({ cart, setCart }) {
     useEffect(() => {
@@ -22,15 +25,32 @@ export default function CartPage({ cart, setCart }) {
 
   return (
     <main className="CartPage">
-    <table>
-    <thead>
-    <tr>
-      <th>Product</th>
-      <th>Qty</th>
-      <th>Price</th>
-      <th>Total Price</th>
-    </tr>
-    </thead>
+    <h2>Shopping Bag</h2>
+    <div className="main-container">
+      <div className="table-container">
+        <div className="table-row heading">
+          <div className="row-item">PRODUCT</div>
+          <div className="row-item">PRICE</div>
+          <div className="row-item">QTY</div>
+          <div className="row-item">TOTAL PRICE</div>
+    </div>
+    {cart.lineItems.map(item =>
+    <LineItem
+      lineItem={item}
+      isPaid={cart.isPaid}
+      key={item._id}
+    />
+    )}
+   </div>
+</div>
+    {/* <h2>Shopping Bag</h2>
+    <div className="CartPageTable">
+    <div className="CartPageTableHeader">
+    Item
+    Item Price
+    Quantity
+    Total Price
+    </div>
     <tbody>
     {cart.lineItems.map(item =>
     <LineItem
@@ -40,7 +60,17 @@ export default function CartPage({ cart, setCart }) {
     />
     )}
     </tbody>
-    </table>
+    </div>
+    <div>
+    <h2>Order Summary</h2>
+    <Card>
+    <Card.Body>Sub Total: {cart.orderTotal}</Card.Body>
+    <Card.Body>Shipping: TBD</Card.Body>
+    <Card.Body>Taxes: 0.00</Card.Body>
+    <Card.Body>Total: {cart.orderTotal}</Card.Body>
+    <Button variant="dark">CHECKOUT</Button>
+    </Card>
+    </div> */}
     </main>
   )
 }
