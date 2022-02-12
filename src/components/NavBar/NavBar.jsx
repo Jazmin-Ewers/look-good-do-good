5d646940a0a27fd126a748cf4a3db8d6ea8e2796
Jazmin-Ewers <jazminewers11@gmail.com>
2022-02-12 04:56:59 -0800

    Center div in the middle of homepage img

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import { BsHandbag, BsHeart } from 'react-icons/bs';
 import { IoPersonCircleOutline } from 'react-icons/io5'
 import './NavBar.css';
 
-
+{/* <BsHeart/> */}
 export default function NavBar({ user, setUser }) {
 
   function handleLogOut() {
@@ -17,7 +17,7 @@ export default function NavBar({ user, setUser }) {
 		<Link to="/" style={{textDecoration: "none"}}><h1 className="logo">Look Good Do Good</h1> </Link>
       <ul className="main-nav">
           <li><Link to="/orders"style={{textDecoration: "none"}}><a>Order History</a></Link></li>
-          <li><Link to="/orders/favorites"style={{textDecoration: "none"}}><a><BsHeart/>Favorites</a></Link></li>
+          <li><Link to="/orders/donate"style={{textDecoration: "none"}}><a>Donate</a></Link></li>
           <li><Link to=""style={{textDecoration: "none"}}><a><IoPersonCircleOutline/>Welcome {user.name}!</a></Link></li>
           <li><Link to="/cart"><a><BsHandbag/></a></Link></li>
           <li><Link onClick={() => handleLogOut()} to="" style={{textDecoration: "none"}}><a>Log Out</a></Link></li>
